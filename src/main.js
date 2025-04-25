dc9eb6207e766beeb96a7750300ee0b2978a2150
zachemtebe <ya.sitora11@gmail.com>
2025-04-25 10:15:20 +0500

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import "./style.css"
+import Typed from 'typed.js';
+var options = {
+  strings: ['Text Gradients' , 'Make your headlines stand out', "Design is intelligence made visible"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+};
+
+var typed = new Typed('#element', options);
